e976eee10ab422955fbb1915652e8a0da6471897
gauhannah <hannahgautreau@gmail.com>
2019-01-21 11:13:25 -0500

    Add files via upload

---
+++ schools.py
@@ -0,0 +1,158 @@
+allSchools = [
+		'bcit',
+		'camosun', 
+		'capU', 
+		'douglascollege', 
+		'KPU', 
+		'langara', 
+		'NYIT', 
+		'OkanaganCollege',
+		'simonfraser', 
+		'tru',
+		'UFV', 
+		'UBC',
+		'UNBC',
+		'uvic',
+		'VIU', 
+		'ubco',
+		'AthabascaUniversity', 
+		'uAlberta',
+		'UCalgary',
+		'uleth',
+		'ACAD',
+		'NAIT',
+		'SAIT',
+		'University_Of_Regina', 'usask',
+		'umanitoba', 'uwinnipeg',
+		'AlgomaU',
+		'brocku', 
+		'CarletonU',
+		'uoguelph',
+		'lakeheadu',
+		'laurentian',
+		'McMaster',
+		'nipissingu',
+		'OCADU',
+		'geegees', #uottawa
+		'queensuniversity',
+		'ryerson',
+		'UofT',
+		'TrentUniversity',
+		'uoit',
+		'uwaterloo',
+		'uwo',
+		'wlu',
+		'UWindsor',
+		'yorku',
+		'Conestoga',
+		'BishopUniversity',
+		'Concordia',
+		'etsmtl',
+		'PolyMTL',
+		'HEC',
+		'mcgill',
+		'UdeM',
+		'usherbrooke',
+		'UQAM',
+		'UQAC',
+		'ulaval',
+		'memorialuniversity',
+		'unb','mta','umoncton',
+		'Dalhousie', 'AcadiaU', 'SMUHalifax', 
+		'stfx',
+		'MSVU', 'cbu','UPEI'
+]
+
+
+
+bcSubs = ['bcit',
+		'camosun', 
+		'capU', 
+		'douglascollege', 
+		'KPU', 
+		'langara', 
+		'NYIT', 
+		'OkanaganCollege',
+		'simonfraser', 
+		'tru',
+		'UFV', 
+		'UBC',
+		'UNBC',
+		'uvic',
+		'VIU', 
+		'ubco'] 
+
+albertaSubs = ['AthabascaUniversity', 
+			'uAlberta',
+			'UCalgary',
+			'uleth',
+			'ACAD',
+			'NAIT',
+			'SAIT']
+
+saskSubs = ['University_Of_Regina', 'usask']
+
+manitobaSubs = ['umanitoba', 'uwinnipeg']
+
+ontarioSubs = ['AlgomaU',
+			'brocku', 
+			'CarletonU',
+			'uoguelph',
+			'lakeheadu',
+			'laurentian',
+			'McMaster',
+			'nipissingu',
+			'OCADU',
+			'geegees', #uottawa
+			'queensuniversity',
+			'ryerson',
+			'UofT',
+			'TrentUniversity',
+			'uoit',
+			'uwaterloo',
+			'uwo',
+			'wlu',
+			'UWindsor',
+			'yorku',
+			'Conestoga']
+
+quebecSubs = ['BishopUniversity',
+			'Concordia',
+			'etsmtl',
+			'PolyMTL',
+			'HEC',
+			'mcgill',
+			'UdeM',
+			'usherbrooke',
+			'UQAM',
+			'UQAC',
+			'ulaval'
+			]
+
+nfldsubs = ['memorialuniversity']
+
+nbSubs = ['unb','mta','umoncton']
+
+nsSubs = ['Dalhousie', 'AcadiaU', 'SMUHalifax', 
+		'stfx',
+		'MSVU', 'cbu']
+
+peiSub = ['UPEI']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
